b0e7b6189e381c67acb6a0735f6e233fae4910ff
Mare Alexandra-Camelia <marekami234@gmail.com>
2024-12-18 16:55:30 +0200

    Add project files.

---
+++ MareAlexandraCameliaLab7/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MareAlexandraCameliaLab7
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MareAlexandraCameliaLab7/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MareAlexandraCameliaLab7
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
